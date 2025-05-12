3b3b902cdabd1dd545d794d5c46cfa1259e1a455
hdz-kevin <kevinhdez592@gmail.com>
2025-05-12 15:44:17 -0600

    add create_cats_table migration

---
+++ database/migrations/2025_05_12_212630_create_cats_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\User;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cats', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class)->nullable();
+            $table->boolean('is_adopted')->default(false);
+            $table->string('name');
+            $table->string('breed');
+            $table->tinyInteger('age', unsigned: true);
+            $table->string('picture');
+            $table->boolean('vaccinated');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cats');
+    }
+};
