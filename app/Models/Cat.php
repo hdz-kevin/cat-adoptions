d855212ab111ca635f26e2cd08d073f02c341ec8
hdz-kevin <kevinhdez592@gmail.com>
2025-05-12 16:00:21 -0600

    add Cat model

---
+++ app/Models/Cat.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cat extends Model
+{
+    use HasFactory;
+
+    protected array $fillable = [
+        'user_id',
+        'is_adopted',
+        'name',
+        'breed',
+        'age',
+        'picture',
+        'vaccinated',
+    ];
+}
